b378b08ff5a0a80695561fdde271f55a4d2cab1b
Taylor <taylorlange95@gmail.com>
2023-11-15 15:16:32 -0600

    pushed finished code.

--- dog-age-calculator.js
+++ dog-age-calculator.js
@@ -1,19 +1,36 @@
-let dogName = process.argv[2];
-
-
-function calculateDogYears(humanYears) {
+function calculateDogAge(humanYears) {
     let dogYears;
-
+  
     if (humanYears === 1) {
-        dogYears = 15
+      dogYears = 15;
+    } else if (humanYears === 2) {
+      dogYears = 15 + 9;
+    } else {
+      dogYears = 15 + 9 + (humanYears - 2) * 5;
     }
-    else if (humanYears === 2) {
-        dogYears = 15 + 9
-    }
-    else {
-        dogYears = 15 + 9 + (humanYears - 2) * 5
-    }
-    return dogYears
-};
+  
+    return dogYears;
+  }
+  
+  function dogAgeSentence(name, humanYears) {
+    let dogYears = calculateDogAge(humanYears);
+    return `Your dog, ${name}, is ${humanYears} years old, but that's ${dogYears} years old in dog years!`;
+  }
+  
+
+  let dogName = process.argv[2];
+  let humanAge = parseInt(process.argv[3]);
+  
+  if (isNaN(humanAge) || humanAge < 1) {
+    console.log("Please provide a valid age for your dog.");
+  } else {
+    console.log(dogAgeSentence(dogName, humanAge));
+  }
+  
+
+
+
+
+
 
 
